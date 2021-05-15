feaff0814d84353da3256307b4b856c48e5c6400
DewEnforcer <piranha863@gmail.com>
2021-05-15 11:32:35 +0200

    Changed the hls video stream source

--- src/components/MoviePlayer.jsx
+++ src/components/MoviePlayer.jsx
@@ -6,7 +6,7 @@ export default class MoviePlayer extends Component {
     render() {
         return (
             <div className="video_container">
-                <ShakaPlayer style={{height: "100vh"}} autoPlay src={'http://demo.unified-streaming.com/video/tears-of-steel/tears-of-steel.ism/.m3u8'}/>
+                <ShakaPlayer style={{height: "100vh"}} autoPlay src={'https://multiplatform-f.akamaihd.net/i/multi/will/bunny/big_buck_bunny_,640x360_400,640x360_700,640x360_1000,950x540_1500,.f4v.csmil/master.m3u8'}/>
             </div>
         )
     }
