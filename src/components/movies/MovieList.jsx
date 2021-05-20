a95694a70665ffe0e63addb36874bef2995d773e
DewEnforcer <piranha863@gmail.com>
2021-05-20 16:17:59 +0200

    Added pagination to movielist, changed styling for movielist component

--- src/components/movies/MovieList.jsx
+++ src/components/movies/MovieList.jsx
@@ -1,17 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import MovieDisplay from './MovieDisplay'
 
 const MAX_LIST_RESULTS = 8;
 
 export default function MovieList({title, data, titleWithoutResults = true}) {
+    const [page, setPage] = useState(1);
+    const [displayedResults, setDisplayedResults] = useState([]);
+    const [onLastPage, setOnLastPage] = useState(false);
+
     const displayTitle = title && (titleWithoutResults || data.length > 0);
+
+    useEffect(() => {
+        let start = (page - 1) * MAX_LIST_RESULTS;
+        
+        if (start + MAX_LIST_RESULTS > data.length) {
+            start = data.length - MAX_LIST_RESULTS;
+            setOnLastPage(true);
+        } else setOnLastPage(false);
+
+        const items = [...data].splice(start, MAX_LIST_RESULTS);
+        setDisplayedResults(items);
+    }, [page, data]);
+
+    const handlePageChange = pageWay => {
+        const newPage = page + pageWay;
+
+        if (newPage < 1) return;
+        if (pageWay === 1 && onLastPage) return; 
+        
+        if (onLastPage) setOnLastPage(false);
+
+        setPage(newPage);
+    }
 
     return (
         <div className="movie_list_box">
             {displayTitle && <h2>{title}</h2>}
             <div className="movie_list_content">
-                {data.splice(0,MAX_LIST_RESULTS).map(m => <MovieDisplay key={m.id} data={m} imgSrc={m.poster_path}/>) /* add img src */}
+                {displayedResults.map(m => <MovieDisplay key={m.id} data={m} imgSrc={m.poster_path}/>) /* add img src */}
             </div>
+            <button className="movie_list_btn movie_list_goback" disabled={page === 1} onClick={() => handlePageChange(-1)}>&#60;</button>
+            <button className="movie_list_btn movie_list_goforward" disabled={onLastPage} onClick={() => handlePageChange(1)}>&#62;</button>
         </div>
     )
 }
