1eee1ce7b5367989d801d5ef998a3f04ed57dbdd
DewEnforcer <piranha863@gmail.com>
2021-05-28 09:47:04 +0200

    Removed leftover addKey function in httpservice

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -6,8 +6,6 @@ axios.defaults.params = {};
 axios.defaults.params["api_key"] = config.api_key;
 axios.defaults.params["language"] = config.app_lang;
 
-export const addApiKey = url => `${url}?api_key=${config.api_key}`;
-
 const http = {
     get: axios.get,
     post: axios.post,
