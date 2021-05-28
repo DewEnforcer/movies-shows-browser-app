62061b2b55f68a094addab59b58712b54294b4a2
DewEnforcer <piranha863@gmail.com>
2021-05-28 09:56:22 +0200

    Removed hardcoded api key from defaults

--- src/config/default.js
+++ src/config/default.js
@@ -1,6 +1,6 @@
 const def_data = {
     app_name: "Movie Browser App",
-    api_key: "c8a3c8ff4ff7d0d247812d2501a395d1",
+    api_key: process.env.REACT_APP_API_KEY,
     API_BASE_URL: "https://api.themoviedb.org/3",
     app_lang: "en-US"
 };
